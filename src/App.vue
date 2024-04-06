f852a4506f3c8eda023bf4e2dca0cf60295ccf61
Adrian Mincu <61752094+skpha13@users.noreply.github.com>
2024-04-06 14:30:26 +0300

    feat: done dropwdown

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,35 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import DropdownCarBrand from "@/components/DropdownCarBrand.vue";
+
+const marcaMasina = [
+    "audi",
+    "bmw",
+    "renault",
+    "dacia"
+]
+
+const brandMarcaMasina = [
+  "Q4",
+  "Etron",
+  "TT",
+  "RS"
+]
+
+const getCarsFromBrand = async () => {
+  console.log(brandMarcaMasina);
+  return brandMarcaMasina;
+}
+
+
 </script>
 
 <template>
+  <div class="flex flex-row justify-center items-center
+              border border-purple-500 rounded-full p-2">
+    <DropdownCarBrand @has-chosen-brand="getCarsFromBrand" :car-brands="marcaMasina" class="mr-10"></DropdownCarBrand>
+    <p>Implement Search</p>
+  </div>
+
   <RouterView />
 </template>
