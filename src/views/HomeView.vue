cc2ea8c6a26bc5936adae97648c39b0efc99e91e
Adrian Mincu <61752094+skpha13@users.noreply.github.com>
2024-04-07 00:56:21 +0300

    feat: done map markers

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,17 +18,25 @@ const fetchInitialBatteries = async () => {
 
 fetchInitialBatteries()
 // ===============================================
+
+const openModal = ref(false);
+const idModel = ref("")
+const openPopup = async (id: string) => {
+  idModel.value = id;
+  openModal.value = true;
+}
 </script>
+
 <template>
   <div class="flex flex-col-reverse md:flex-row md:h-full md:max-h-screen">
     <div v-if="!store.isFiltered"
          class="flex flex-row flex-wrap
          w-full md:w-3/4 md:max-h-fit md:overflow-y-scroll">
-        <Battery v-for="battery in batteries" :battery="battery" />
+        <Battery @clicked-battery="id => openPopup(id)" v-for="battery in batteries" :battery="battery" />
     </div>
     <div v-else class="flex flex-row flex-wrap
                 w-full md:w-3/4 md:max-h-fit md:overflow-y-scroll">
-        <Station v-for="station in store.stations" :battery-list="station.batteries" :name="station.address" />
+        <Station @clicked-station="id => openPopup(id)" v-for="station in store.stations" :battery-list="station.batteries" :name="station.address" />
     </div>
     <MapComp class="rounded-lg my-4 z-10"></MapComp>
   </div>
