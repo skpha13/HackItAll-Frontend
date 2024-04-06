a63e30d16f09b93c71339f42cbcd9f5cf84c98fa
anarosu <anarosu766@gmail.com>
2024-04-06 14:23:38 +0300

    trying to upload a map

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import 'leaflet/dist/leaflet.css';
+import 'vue-map-ui/dist/normalize.css';
+import 'vue-map-ui/dist/style.css';
+import 'vue-map-ui/dist/theme-all.css';
 </script>
 
 <template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import NotFound from "@/views/404PageView.vue"
 import Home from "@/views/HomeView.vue";
+import Map from "@/views/MapView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path:'/map',
+      name: `Map`,
+      component: Map
     },
     {
       path: '/:pathMatch(.*)*',
